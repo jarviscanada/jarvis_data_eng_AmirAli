adcb24ffb598da5c8379b7a415abecd9d3c3161d
Amir Ali <amir.a.23@hotmail.com>
2022-11-09 01:53:05 +0000

    Remove comments and extra lines

---
+++ linux_sql/scripts/psql_docker.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+cmd=$1
+db_username=$2
+db_password=$3
+sudo systemctl status docker || systemctl ...
+docker container inspect jrvs-psql
+container_status=$?
+case $cmd in
+  create)
+  if [ $container_status -eq 0 ]; then
+		echo 'Container already exists'
+		exit 1
+	fi
+  if [ $# -ne 3 ]; then
+    echo 'Create requires username and password'
+    exit 1
+  fi
+	docker volume ....
+	docker run ....
+	exit $?
+	;;
+  start|stop)
+  if [ $container_status ...
+  ...
+	docker container $cmd jrvs-psql
+	exit $?
+	;;
+  *)
+	echo 'Illegal command'
+	echo 'Commands: start|stop|create'
+	exit 1
+	;;
+esac
