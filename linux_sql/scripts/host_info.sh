12da651a759c09a6ea3ac759734df62a7d1c6c9d
Amir Ali <amir.a.23@hotmail.com>
2022-11-10 21:13:37 +0000

    Add hardware specification data parsing logic

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -3,3 +3,18 @@ psql_port=$2
 db_name=$3
 psql_user=$4
 psql_password=$5
+
+#parse info
+lscpu_out=$()lscpu)
+hostname=$(hostname -f)
+cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out" | egrep "Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out" | egrep "Model name:.*" | xargs | cut -d " " -f3-5)
+cpu_mhz=$(echo "$lscpu_out" | egrep "CPU MHz:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out" | egrep "L2 cache:" | awk '{print $3}' | head -c-2 | xargs)
+total_mem= $(grep MemTotal /proc/meminfo | awk '{print $2}' | xargs)
+timestamp= $(vmstat -t | awk 'FNR == 3 {print $18,$19}')
+
+#insert statement variable
+#instert into db
+#
