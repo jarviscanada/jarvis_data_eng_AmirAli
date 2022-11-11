f39d6a0e6dd6f5b7432ee853b16593cd0fc25c9d
Amir Ali <amir.a.23@hotmail.com>
2022-11-11 00:02:52 +0000

    Add variables for storing cli args

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 #set up variables for cli arguments
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
 #validate for number of args
 #save machine statistics in MB
 #save current hostname to variable
