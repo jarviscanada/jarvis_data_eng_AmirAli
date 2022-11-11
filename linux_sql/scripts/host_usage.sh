1a183c562e42a8543cfd4dd5d22f3880c11278ba
Amir Ali <amir.a.23@hotmail.com>
2022-11-11 00:52:30 +0000

    Fix bug parsing usage data and reformatted insert stmt

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -23,17 +23,31 @@ hostname=$(hostname -f)
 #current timestamp in `2019-11-26 14:40:19` format
 timestamp=$(vmstat -t | awk 'FNR == 3 {print $18,$19}')
 memory_free=$(echo "$vmstat_mb" | awk 'FNR == 3 {print $4}' | xargs)
-cpu_idle=$(echo "$vmstat_mb" | awk 'FNR == 3 {print $15} | xargs')
-cpu_kernel=$(echo "$vmstat_mb" | awk 'FNR == 3 {print $14} | xargs')
+cpu_idle=$(echo "$vmstat_mb" | awk 'FNR == 3 {print $15}' | xargs)
+cpu_kernel=$(echo "$vmstat_mb" | awk 'FNR == 3 {print $14}' | xargs)
 disk_io=$(vmstat -d | awk 'FNR == 3 {print $10}' | xargs)
 disk_available=$(df -BM / | awk 'FNR == 2 {print $4}' | head -c -2 | xargs)
 
 #find host_id to related host_info table entry
-host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')"
 
 #insert statement for usage data stored in a variable
-insert_stmt="INSERT INTO host_usage(timestamp,host_id,memory_free,cpu_idle,cpu_kernel,disk_io,disk_available)
-VALUES('$timestamp','$host_id','$memory_free','$cpu_idle','$cpu_kernel','$disk_io','$disk_available')"
+insert_stmt="INSERT INTO host_usage(
+  timestamp,
+  host_id,
+  memory_free,
+  cpu_idle,
+  cpu_kernel,
+  disk_io,
+  disk_available
+  )VALUES(
+    '$timestamp',
+    $host_id,
+    $memory_free,
+    $cpu_idle,
+    $cpu_kernel,
+    $disk_io,
+    $disk_available)"
 
 #set up env for psql authentication
 export PGPASSWORD=$psql_password
