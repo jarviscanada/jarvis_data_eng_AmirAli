36563d0251ad786092e96271604b0631c0c51b4a
Amir Ali <amir.a.23@hotmail.com>
2022-11-11 00:01:20 +0000

    Set up structure in comments

---
+++ linux_sql/scripts/host_usage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#set up variables for cli arguments
+#validate for number of args
+#save machine statistics in MB
+#save current hostname to variable
+#retieve resource usage data into variables
+#timestamp in UTC format
+#find host_id to related host_info table entry
+#insert statement for usage data stored in a variable
+#set up env for psql authentication
+#insert usage data into database
