39912372f1bdf94c0b506afca816ea0b613ddc84
Amir Ali <amir.a.23@hotmail.com>
2022-11-11 00:04:26 +0000

    Add validation for number of cli args

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -6,7 +6,12 @@ psql_port=$2
 db_name=$3
 psql_user=$4
 psql_password=$5
+
 #validate for number of args
+if [ "$#" -ne 5 ]; then
+    echo "Please enter the correct number of parameters"
+    exit 1
+fi
 #save machine statistics in MB
 #save current hostname to variable
 #retieve resource usage data into variables
