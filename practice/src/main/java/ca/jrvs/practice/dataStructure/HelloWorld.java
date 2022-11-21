c2db6b1d2ef439a059f04117df2c96175e20ba10
Amir Ali <amir.a.23@hotmail.com>
2022-11-21 22:37:49 +0000

    Add Hello World

--- practice/src/main/java/ca/jrvs/practice/dataStructure/HelloWorld.java
+++ practice/src/main/java/ca/jrvs/practice/dataStructure/HelloWorld.java
@@ -1,5 +1,11 @@
+package ca.jrvs.practice.dataStructure;
+
+import static java.lang.System.out;
+
 class HelloWorld {
-  public static void main(String args[]) {
-    System.out.println("Hello, World");
+
+  public static void main(String[] args) {
+
+    out.println("Hello, World");
   }
 }
