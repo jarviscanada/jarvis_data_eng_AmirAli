6a00a16fe6386b159efdf01376ea7d409d2e60d0
Amir Ali <amir.a.23@hotmail.com>
2022-11-21 04:14:50 +0000

    Implement matchJpeg function

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
@@ -0,0 +1,40 @@
+package ca.jrvs.apps.practice;
+
+import java.util.regex.*;
+
+public class RegexExcImp implements RegexExc{
+
+  /**
+   *return true if filename extension is jpg or jpeg (case-insensitive)
+   * @param filename
+   * @return
+   */
+  @Override
+  public boolean matchJpeg(String filename) {
+    String jpg = "jpg";
+    String jpeg = "jpeg";
+    boolean isMatched = Pattern.matches("(?i)(\\.jpg|\\.jpeg)", filename);
+    return isMatched;
+  }
+
+  /**
+   * return true if ip is valid
+   * to simplify the problem IP address range is from 0.0.0.0 to 999.999.999.999
+   * @param ip
+   * @return
+   */
+  @Override
+  public boolean matchIp(String ip) {
+    return false;
+  }
+
+  /**
+   * return true if lineis empty (e.g. empty, white space, tabs, etc..)
+   * @param line
+   * @return
+   */
+  @Override
+  public boolean isEmptyLine(String line) {
+    return false;
+  }
+}
